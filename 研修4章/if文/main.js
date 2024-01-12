d7552094319e3fb73f347378e7e1f0487766d455
yuuki106 <117831203+yuuki106@users.noreply.github.com>
2024-01-12 20:45:46 +0900

    Add files via upload

---
+++ 研修4章/if文/main.js
@@ -0,0 +1,15 @@
+
+
+var boolean = true;
+if (boolean) {
+  console.log(boolean)
+}
+var num = 100;
+if (num === 100) {
+  console.log(num);
+}
+var text = "JavaScript";
+if (text === "JavaScript") {
+  console.log(text);
+}
+
---
+++ 研修4章/三項演算子/main.js
@@ -0,0 +1,41 @@
+// var boolean = true;
+// var resultText = "";
+// if (boolean) {
+// resultText = "Aの処理"
+// } else {
+// resultText = "Bの処理"
+// };
+// console.log(resultText);
+
+// var resultText = boolean ? "Aの処理" : "Bの処理";
+// console.log(resultText)
+
+// var resultNum = 5 > 2 ? true : false;
+// console.log(resultNum)
+
+
+// 1. 変数の中にtrueまたは、falseが代入された時の処理を作ってください。
+var boolean = false;
+var booleanResult = boolean ? "処理が実行されました。":"処理がキャンセルされました。"
+console.log(booleanResult);
+
+// 2. 代入された変数の中が5より大きい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。
+var num1 = 5;
+var checkNum1 = num1 > 5 ? "num1は5より大きいです": "num1は5以下です";
+console.log(checkNum1);
+
+// 3. 代入された変数の中が5より小さい時にtrueの処理と、5より小さい時にfalseの処理を作ってくださ
+// い。
+var num2 = 5;
+var checkNum2 = num2 < 5 ? "num2は5より小さいです": "num2は5以上です";
+console.log(checkNum2);
+
+// 4. 代入された変数の中が10以上の時にtrueの処理と、10以下の時にfalseの処理を作ってください。
+var num3 = 10;
+var checkNum3 = num3 >= 10 ? "num3は10以上です。": "num3は10より小さいです。";
+console.log(checkNum3);
+
+// 5. 代入された変数の中に値が存在する場合はtrue、存在しない場合はfalseの処理にしてください。
+var value = "";
+var checkValue = (value) ? "値が存在します。": "値が存在しません。";
+console.log(checkValue);
