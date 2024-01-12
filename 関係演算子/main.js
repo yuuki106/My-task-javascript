07e777f721c8517b28e8806faec2456ab92e156a
yuuki106 <117831203+yuuki106@users.noreply.github.com>
2024-01-12 22:11:33 +0900

    Add files via upload

---
+++ 関係演算子/main.js
@@ -0,0 +1,41 @@
+// var x = 5
+// var y = 3
+// if (x > y) {
+// console.log("xはyより大きい");
+// }
+
+// var x = 5
+// var y = 5
+// if (x >= y) {
+// console.log("xはyより大きいまたは等しい");
+// }
+
+
+
+// 1. xがyより大きい時にifの処理へ入るようにしてください。
+var x = 15;
+var y = 10;
+if(x > y ){
+  console.log("xはyより大きいです。");
+}
+
+// 2. xがyより小さい時にifの処理へ入るようにしてください。
+var x = 5;
+var y = 10;
+if(x < y ){
+  console.log("xはyより小さいです。");
+}
+
+// 3. xがy以上の時にifの処理へ入るようにしてください。
+var x = 15;
+var y = 10;
+if (x >= y){
+  console.log("xはy以上です");
+}
+
+// 4. xがy以下の時にifの処理へ入るようにしてください。
+var x = 5;
+var y = 10;
+if (x <= y){
+  console.log("xはy以下です");
+}
